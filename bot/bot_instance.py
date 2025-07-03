8a4242a101fab1b6eb63e29225653a961d9aa4c2
Na-seunghyun <looloo90@naver.com>
2025-07-03 14:17:45 +0900

    Update bot_instance.py

--- bot/bot_instance.py
+++ bot/bot_instance.py
@@ -1,9 +1,9 @@
 import os
 import discord
 from discord.ext import commands
-from bot import events
-from bot.commands import check_nickname, summon, team_split, move_to_eat, voice_ranking
-from bot.utils.constants import INTENTS, GUILD_ID
+from bot import events  # 이벤트 등록
+from bot.commands import check_nickname, summon, team_split, move_to_eat, voice_ranking  # 명령어 임포트
+from bot.utils.constants import INTENTS
 
 bot = commands.Bot(command_prefix="!", intents=INTENTS)
 tree = bot.tree
