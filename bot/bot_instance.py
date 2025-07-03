f0ea527b1d5e39c20bbf9f0878b79dda50a83959
Na-seunghyun <looloo90@naver.com>
2025-07-03 14:16:30 +0900

    Update bot_instance.py

--- bot/bot_instance.py
+++ bot/bot_instance.py
@@ -1,1 +1,16 @@
+import os
+import discord
+from discord.ext import commands
+from bot import events
+from bot.commands import check_nickname, summon, team_split, move_to_eat, voice_ranking
+from bot.utils.constants import INTENTS, GUILD_ID
 
+bot = commands.Bot(command_prefix="!", intents=INTENTS)
+tree = bot.tree
+
+def run_bot():
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    if TOKEN:
+        bot.run(TOKEN)
+    else:
+        print("❌ 환경변수 DISCORD_TOKEN이 없습니다.")
