2c3a4052eeca683bae156aa4ad239f7fa0608bd6
Na-seunghyun <looloo90@naver.com>
2025-07-03 14:43:41 +0900

    Update bot_instance.py

--- bot/bot_instance.py
+++ bot/bot_instance.py
@@ -1,14 +1,30 @@
 import os
-import discord
 from discord.ext import commands
-from bot import events  # 이벤트 등록
-from bot.commands import check_nickname, summon, team_split, move_to_eat, voice_ranking  # 명령어 임포트
-from bot.utils.constants import INTENTS
+from discord import app_commands
+from keep_alive import keep_alive
 
-bot = commands.Bot(command_prefix="!", intents=INTENTS)
+import discord
+
+# 봇 초기화, 싱글톤 역할, 토큰 실행 함수 포함
+
+intents = discord.Intents.default()
+intents.members = True
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 tree = bot.tree
 
+GUILD_ID = 1309433603331198977
+
+# Supabase 초기화 코드 (env에서 불러오기)
+from supabase import create_client, Client
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 def run_bot():
+    keep_alive()
+
     TOKEN = os.getenv("DISCORD_TOKEN")
     if TOKEN:
         bot.run(TOKEN)
