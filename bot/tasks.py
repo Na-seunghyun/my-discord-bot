73c344f2cef50e6cc05dbe3d334ce7430551f43d
Na-seunghyun <looloo90@naver.com>
2025-07-03 14:19:47 +0900

    Update tasks.py

--- bot/tasks.py
+++ bot/tasks.py
@@ -1,1 +1,18 @@
 
+import discord
+from discord.ext import tasks
+from bot.bot_instance import bot
+from bot.utils.constants import MONITORED_CHANNEL_NAMES
+
+@tasks.loop(minutes=30)
+async def check_voice_channels_for_streaming():
+    for guild in bot.guilds:
+        text_channel = discord.utils.get(guild.text_channels, name="자유채팅방")
+        if not text_channel:
+            continue
+
+        for vc in guild.voice_channels:
+            if vc.name in MONITORED_CHANNEL_NAMES and vc.members:
+                non_bot_members = [m for m in vc.members if not m.bot]
+                if not any(m.voice and m.voice.self_stream for m in non_bot_members):
+                    mentions = " ".join(m.mention for m
