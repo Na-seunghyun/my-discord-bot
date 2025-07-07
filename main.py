3ffe67b6c11217d3d2d061faaa27a6c141c64deb
Na-seunghyun <looloo90@naver.com>
2025-07-07 14:33:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,20 +16,19 @@ from supabase import create_client, Client
 import uuid  # uuid 추가
 from dotenv import load_dotenv
 
-load_dotenv()
-
-
 
 
 KST = timezone(timedelta(hours=9))
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+if __name__ == "__main__":
+    load_dotenv()
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-print("SUPABASE_URL:", SUPABASE_URL)
-print("SUPABASE_KEY:", SUPABASE_KEY)
+    print("SUPABASE_URL:", SUPABASE_URL)
+    print("SUPABASE_KEY:", SUPABASE_KEY)
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 GUILD_ID = 1309433603331198977
 MONITORED_CHANNEL_NAMES = [f"일반{i}" for i in range(1, 17)] + ["큰맵1", "큰맵2"]
